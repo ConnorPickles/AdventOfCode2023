1bbb90720ab5b135a0d8dca59d71864d4ebec963
ConnorPickles <cpickles98@gmail.coim>
2023-12-21 21:14:25 -0800

    day 10 part 2

--- day10/src/aoc.cpp
+++ day10/src/aoc.cpp
@@ -18,6 +18,10 @@ class Tile {
         return row == other.row && col == other.col;
     }
 
+    bool operator!=(const Tile& other) const {
+        return !(*this == other);
+    }
+
     void next(Tile& prev, char symbol) {
         Tile result;
 
@@ -69,23 +73,35 @@ Tile findStartingTile(const std::vector<std::string>& input) {
     exit(1);
 }
 
-int main(int argc, char** argv) {
-    std::vector<std::string> input = aoc::parseInput(argv[1]);
+Tile findStartingPath(const Tile& start, std::vector<std::string>& input) {
+    Tile result;
+    Tile second;
 
-    Tile start = findStartingTile(input);
-
-    // find the 2 paths leading away from the starting tile
-    Tile path1;
-    Tile path2;
+    if (start.row - 1 >= 0) {
+        switch (input[start.row - 1][start.col]) {
+        case '|':
+        case 'F':
+        case '7': {
+            Tile tmp(start.row - 1, start.col);
+            second = result;
+            result = std::move(tmp);
+            break;
+        }
+        default:
+            break;
+        }
+    }
 
     if (start.col - 1 >= 0) {
         switch (input[start.row][start.col - 1]) {
         case '-':
         case 'F':
-        case 'L':
-            path1.row = start.row;
-            path1.col = start.col - 1;
+        case 'L': {
+            Tile tmp(start.row, start.col - 1);
+            second = result;
+            result = std::move(tmp);
             break;
+        }
         default:
             break;
         }
@@ -97,21 +113,8 @@ int main(int argc, char** argv) {
         case '7':
         case 'J': {
             Tile tmp(start.row, start.col + 1);
-            (path1.initialized()) ? path2 = std::move(tmp) : path1 = std::move(tmp);
-            break;
-        }
-        default:
-            break;
-        }
-    }
-
-    if (start.row - 1 >= 0) {
-        switch (input[start.row - 1][start.col]) {
-        case '|':
-        case 'F':
-        case '7': {
-            Tile tmp(start.row - 1, start.col);
-            (path1.initialized()) ? path2 = std::move(tmp) : path1 = std::move(tmp);
+            second = result;
+            result = std::move(tmp);
             break;
         }
         default:
@@ -125,23 +128,120 @@ int main(int argc, char** argv) {
         case 'L':
         case 'J': {
             Tile tmp(start.row + 1, start.col);
-            (path1.initialized()) ? path2 = std::move(tmp) : path1 = std::move(tmp);
+            second = result;
+            result = std::move(tmp);
+            break;
         }
         default:
             break;
         }
     }
 
-    // follow both of the paths until they converge
-    Tile prevPath1 = start;
-    Tile prevPath2 = start;
-    for (int i = 1;; i++) {
-        if (path1 == path2 || prevPath1 == path2) {
-            std::cout << i << std::endl;
-            break;
-        }
-
-        path1.next(prevPath1, input[path1.row][path1.col]);
-        path2.next(prevPath2, input[path2.row][path2.col]);
+    // change the S to its appropriate symbol to make our lives easier when determining boundary crossing later
+    if (result.row == second.row) {
+        input[start.row][start.col] = '-';
+    } else if (result.col == second.col) {
+        input[start.row][start.col] = '|';
+    } else if (result.row < second.row && result.col > second.col) {
+        input[start.row][start.col] = 'L';
+    } else if (result.row < second.row && result.col < second.col) {
+        input[start.row][start.col] = 'J';
+    } else if (result.row > second.row && result.col > second.col) {
+        input[start.row][start.col] = '7';
+    } else if (result.row > second.row && result.col < second.col) {
+        input[start.row][start.col] = 'F';
     }
+
+    return result;
+}
+
+bool crossedVerticalBoundary(char curr, char& startChar) {
+    if (curr == '|') {
+        return true;
+    }
+
+    if (curr == '-') {
+        return false;
+    }
+
+    if (curr == 'F') {
+        startChar = 'F';
+        return false;
+    }
+
+    if (curr == 'L') {
+        startChar = 'L';
+        return false;
+    }
+
+    if (curr == '7') {
+        if (startChar == 'F') {
+            startChar = 'u';
+            return false;
+        } else {
+            startChar = 'u';
+            return true;
+        }
+    }
+
+    if (curr == 'J') {
+        if (startChar == 'L') {
+            startChar = 'u';
+            return false;
+        } else {
+            startChar = 'u';
+            return true;
+        }
+    }
+
+    std::cout << "Could not determine if a boundary was crossed" << std::endl;
+    exit(1);
+}
+
+bool tileInLoop(const Tile& t, const std::vector<Tile>& loopTiles) {
+    for (const Tile& curr : loopTiles) {
+        if (t == curr) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int main(int argc, char** argv) {
+    std::vector<std::string> input = aoc::parseInput(argv[1]);
+    std::vector<Tile> loopTiles;
+    Tile start = findStartingTile(input);
+    Tile curr = findStartingPath(start, input);
+    Tile prev = start;
+    loopTiles.push_back(start);
+    loopTiles.push_back(curr);
+
+    while (curr != start) {
+        curr.next(prev, input[curr.row][curr.col]);
+        loopTiles.push_back(curr);
+    }
+
+    int insideTiles = 0;
+    for (int i = 0; i < input.size(); i++) {
+        bool inside = false;
+        char startChar = 'u';
+        int tilesOnRow = 0;
+        for (int j = 0; j < input[0].size(); j++) {
+            if (tileInLoop(Tile(i, j), loopTiles) == false) {
+                if (inside) {
+                    insideTiles++;
+                    tilesOnRow++;
+                }
+
+                continue;
+            }
+
+            if (crossedVerticalBoundary(input[i][j], startChar)) {
+                inside = !inside;
+            }
+        }
+    }
+
+    std::cout << insideTiles << std::endl;
 }
